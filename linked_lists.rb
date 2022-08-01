ef88d157a1f967a693d8286ad790984b0790fe3a
Schifferking <schifferking@hotmail.com>
2022-08-01 12:26:04 -0500

    Add basic method to LinkedList class
    Some of the basic operations for this class are to get the tail of the
    linked list (last node of the list) and append a node at the end of
    the list. Also another one is to get the head of the linked list (the
    first node of the list), but this method was added when the LinkedList
    class was created.

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,20 +1,30 @@
-class Linked_list
-  def initialize(head=nil)
-    self.head = head
+class LinkedList
+  attr_accessor :head
+
+  def initialize(head = nil)
+    self.head = Node.new(head)
   end
 
-  protected
+  def tail
+    return head if head.next_node.nil?
 
-  attr_accessor :head
+    node = head.next_node
+    node = node.next_node until node.next_node.nil?
+    node
+  end
+
+  def append(value)
+    return self.head = Node.new(value) if head.value.nil?
+
+    tail.next_node = Node.new(value)
+  end
 end
 
 class Node
-  def initialize(value=nil, next_node=nil)
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
     self.value = value
     self.next_node = next_node
   end
-
-  protected 
-
-  attr_accessor :value, :next_node
 end
